8392604e197d19a5edfdf31e6f81059894f2f951
Ricardo <1005898152@qq.com>
2017-12-25 14:48:42 +0800

    second

--- app/src/main/java/test/company/apple/test1/MainActivity.java
+++ app/src/main/java/test/company/apple/test1/MainActivity.java
@@ -2,13 +2,16 @@ package test.company.apple.test1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = findViewById(R.id.iv);
         //cool
+
     }
 }
